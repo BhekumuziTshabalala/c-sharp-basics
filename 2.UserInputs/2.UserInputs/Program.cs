d91ea2e2eb82c5c16fc97c749e4a40bcf8110012
Bhekumuzi Tshabalala <Bhekumuzitshabalala.main@gmail.com>
2024-05-01 15:03:09 +0200

    innitial commit

---
+++ 2.UserInputs/2.UserInputs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.UserInputs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            String name = Console.ReadLine();
+
+            Console.Write("Enter Your Surame: ");
+            String surname = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + name + " " + surname);
+
+
+        }
+    }
+}
